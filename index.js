b47398c2e8230c8b8418bf7f162648a41b4686da
David <davidyen1124@gmail.com>
2015-07-10 01:26:31 +0800

    first node.js version

---
+++ index.js
@@ -0,0 +1,21 @@
+var mrt = require('./lib/mrt');
+
+if (process.argv.length !== 3) {
+	console.log('Usage: mrt 台北車站');
+	return;
+}
+
+var name = process.argv[2];
+mrt.findByName(name, function(err, results) {
+	if (err) {
+		console.error('Error:', err);
+	}
+
+	if (!results) {
+		console.error('No available datas');
+	}
+
+	results.forEach(function(item) {
+		console.log(item.destination + ': ' + item.countdown);
+	});
+});
